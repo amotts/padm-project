39d2ead3b84fe9f4d1b643659c38378fdd603929
Andrew Motz <amotz@mit.edu>
2023-12-06 23:36:05 -0500

    file reorg

--- activity_planner_best_first.py
+++ activity_planner_best_first.py
@@ -1,5 +1,5 @@
 ## Create a BFS planner based on the example planner in the pddl_parser library and the Pset 1 informed search BFS implementation
-# python -B -m activity_planner_best_first /home/amotts/Documents/MIT_FA23/padm-project/activity_planning_pddl_domain.pddl /home/amotts/Documents/MIT_FA23/padm-project/activity_planning_pddl_problem.pddl
+# python -B -m activity_planner_best_first activity_planning_pddl_domain.pddl activity_planning_pddl_problem.pddl
 
 
 ## Future plans - implement best first using Priority Queue w/ cost as the path length to satisfy goal if when we perform action, no negative effects
